a449801be31cdea3b13ee1c28931ed724c7611ca
Albaraazain <albaraazain33@gmail.com>
2025-03-07 10:46:37 +0200

    feat: add JSON decoding import to model builder
    - Import `dart:convert` for potential JSON parsing utilities
    - Prepare model builder for enhanced data transformation capabilities
    - Maintain existing builder structure while expanding import support

--- lib/src/builders/model_builder.dart
+++ lib/src/builders/model_builder.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:build/build.dart';
 import 'package:postgres/postgres.dart';
 import 'package:supabase_model_generator/src/generators/enum_generator.dart';
