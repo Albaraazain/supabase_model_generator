5ba87b2254a07a1b349fb8f4bd17395230beb217
Albaraazain <albaraazain33@gmail.com>
2025-03-07 10:46:54 +0200

    refactor: remove unused StringExtension utility
    - Delete `string_extensions.dart` file containing unused `pascalCase` extension
    - Simplify project structure by removing unnecessary utility method
    - Maintain clean and focused codebase by eliminating redundant code

--- lib/src/utils/string_extensions.dart
+++
@@ -1,8 +0,0 @@
-extension StringExtension on String {
-  String get pascalCase {
-    if (isEmpty) return this;
-    return split('_')
-        .map((word) => word.isEmpty ? '' : word[0].toUpperCase() + word.substring(1).toLowerCase())
-        .join();
-  }
-} 
